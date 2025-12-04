328260421a9dd92a0335be7dcff597e802cb635c
anshifaanshi <anshifajamsher@gmail.com>
2025-12-04 23:39:19 +0300

    first commit

---
+++ src/sections/TestimonialSection.jsx
@@ -0,0 +1,44 @@
+import Marquee from "react-fast-marquee";
+import SectionTitle from "../components/SectionTitle";
+import TestimonialCard from "../components/TestimonialCard";
+import { testimonialsData } from "../data/testimonial";
+
+export default function TestimonialSection() {
+    return (
+        <div id="testimonials" className="px-4 md:px-16 lg:px-24 xl:px-32">
+            <SectionTitle 
+                text1="Testimonials" 
+                text2="Don't just take our words" 
+                text3="Hear what our users say about us. We're always looking for ways to improve. If you have a positive experience with us, leave a review." 
+                textHighlightClass="text-[#4682B4]" // Pass Steel Blue for highlights instead of pink
+            />
+
+            {/* Marquee slider */}
+            <Marquee className="max-w-5xl mx-auto mt-11" gradient={true} speed={25} gradientColor="#000">
+                <div className="flex items-center justify-center py-5 overflow-hidden">
+                    {[...testimonialsData, ...testimonialsData].map((testimonial, index) => (
+                        <TestimonialCard 
+                            key={index} 
+                            index={index} 
+                            testimonial={testimonial} 
+                            highlightColor="#4682B4" // Replace pink in card with Steel Blue
+                        />
+                    ))}
+                </div>
+            </Marquee>
+
+            <Marquee className="max-w-5xl mx-auto" gradient={true} speed={25} direction="right" gradientColor="#000">
+                <div className="flex items-center justify-center py-5 overflow-hidden">
+                    {[...testimonialsData, ...testimonialsData].map((testimonial, index) => (
+                        <TestimonialCard 
+                            key={index} 
+                            index={index} 
+                            testimonial={testimonial} 
+                            highlightColor="#4682B4" // Steel Blue
+                        />
+                    ))}
+                </div>
+            </Marquee>
+        </div>
+    );
+}
