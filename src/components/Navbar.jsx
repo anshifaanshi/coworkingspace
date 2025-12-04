d24a45efe8794ac73aea09fcfbe502c36421a2ad
anshifaanshi <anshifajamsher@gmail.com>
2025-12-04 23:46:56 +0300

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,39 +13,43 @@ export default function Navbar() {
 
     return (
         <>
-            <motion.nav className="fixed top-0 z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
+            <motion.nav 
+                className="fixed top-0 z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-32 backdrop-blur"
                 initial={{ y: -100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 viewport={{ once: true }}
                 transition={{ type: "spring", stiffness: 250, damping: 70, mass: 1 }}
             >
-                <Link href="/">
-                    <img className="h-8.5 w-auto" src="/assets/logo.svg" alt="logo" />
+                <Link to="/">
+                    <img className="h-8.5 w-auto" src="" alt="logo" />
                 </Link>
 
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
                     {navlinks.map((link) => (
-                        <a key={link.name} href={link.href} className="hover:text-pink-500 transition">
+                        <a key={link.name} href={link.href} className="hover:text-[#4682B4] transition">
                             {link.name}
                         </a>
                     ))}
                 </div>
 
-                <button className="hidden md:block px-6 py-2.5 bg-pink-600 hover:bg-pink-700 active:scale-95 transition-all rounded-full">
+                <button className="hidden md:block px-6 py-2.5 bg-[#315f7d] hover:bg-[#4682B4] active:scale-95 transition-all rounded-full text-white">
                     Start free trial
                 </button>
                 <button onClick={() => setIsOpen(true)} className="md:hidden">
-                    <MenuIcon size={26} className="active:scale-90 transition" />
+                    <MenuIcon size={26} className="active:scale-90 transition text-[#315f7d]" />
                 </button>
             </motion.nav>
 
             <div className={`fixed inset-0 z-100 bg-black/40 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-400 ${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
                 {navlinks.map((link) => (
-                    <Link key={link.name} href={link.href} onNavigate={() => setIsOpen(false)}>
+                    <Link key={link.name} to={link.href} onClick={() => setIsOpen(false)} className="text-white hover:text-[#4682B4] transition">
                         {link.name}
                     </Link>
                 ))}
-                <button onClick={() => setIsOpen(false)} className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-pink-600 hover:bg-pink-700 transition text-white rounded-md flex">
+                <button 
+                    onClick={() => setIsOpen(false)} 
+                    className="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-[#315f7d] hover:bg-[#4682B4] transition text-white rounded-md flex"
+                >
                     <XIcon />
                 </button>
             </div>
