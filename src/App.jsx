b554ba4dfa7acbe0e7a73beb59a1fe99d37001f1
anshifaanshi <anshifajamsher@gmail.com>
2026-02-11 22:22:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
+
 import "./globals.css";
 import LenisScroll from "./components/LenisScroll";
 
@@ -9,11 +9,12 @@ export default function App() {
     return (
         <>
             <LenisScroll />
-            <Navbar />
+       
             <Routes>
                 <Route path="/" element={<HomePage />} />
             </Routes>
-            <Footer />
+      
         </>
     );
+
 }
